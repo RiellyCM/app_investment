f34bc6506464e4a48dcfb30b51022936e2cc6e61
Rielly <magalhaesrielly@gmail.com>
2020-07-19 23:03:47 -0300

    add mobily adjustment

--- dashboard/position/script.js
+++ dashboard/position/script.js
@@ -13,22 +13,30 @@ async function setPositionValues() {
 
     //add date col
     const dateCol = document.createElement("td");
-    dateCol.innerHTML = position.purchaseDate;
+    dateCol.innerHTML = position.purchasedDate.split("T")[0];
     row.appendChild(dateCol);
 
     //add invested col
     const investedCol = document.createElement("td");
-    investedCol.innerHTML = position.purchaseAmount * position.purchasePrice;
+
+    const invested = position.purchaseAmount * position.purchasePrice;
+    investedCol.innerHTML = invested.toFixed(2);
     row.appendChild(investedCol);
+
+    //add btc
+    const btcCol = document.createElement("td");
+    btcCol.innerHTML = position.currentPrice.toFixed(2);
+    row.appendChild(btcCol);
 
     //add variation
     const variationCol = document.createElement("td");
-    variationCol.innerHTML = position.variation;
+    variationCol.innerHTML = position.variation.toFixed(2);
     row.appendChild(variationCol);
 
     //add grossAmountCol
     const grossAmountCol = document.createElement("td");
-    grossAmountCol.innerHTML = position.currentBtcAmount * position.currentPrice;
+    const grossAmount = position.currentBtcAmount * position.currentPrice;
+    grossAmountCol.innerHTML = grossAmount.toFixed(2);
     row.appendChild(grossAmountCol);
 
     return row;
